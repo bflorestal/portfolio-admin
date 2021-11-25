9e9843b623722854f06b2d66f5dafa7d4ad8b82a
bflorestal <yayou7710@hotmail.fr>
2021-11-25 10:02:58 +0100

    Ajouts et modifs design, début du code js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("Coucou !");
+// Menu burger
+let menu_burger = document.getElementById("menu");
+let show = 0;
+
+function display() {
+    if (show == 0) {
+        menu_burger.style.display = "block";
+        show = 1;
+    } else {
+        menu_burger.style.display = "none";
+        show = 0;
+    }
+}
+
+/**********************AJOUT PROJET**********************/
+function addProj() {
+    let btnAddProj = document.getElementById("btnAddProj");
+    var urlDataProj = "";
+}
+
+/**********************MODIF PROJET**********************/
+function modProj() {
+    let btnModProj = document.getElementById("btnModProj");
+    console.log(btnModProj);
+}
+
+/**********************SUPPR PROJET**********************/
+function rmvProj() {
+    let btnRmvProj = document.getElementById("btnRmvProj");
+    console.log(btnRmvProj);
+}
