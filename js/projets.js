eb689b2be0d53c7a93cfe260a8674caa531f3324
bflorestal <yayou7710@hotmail.fr>
2021-11-26 10:43:44 +0100

    Ajout et suppression Projets administrables fonctionnels

--- js/projets.js
+++ js/projets.js
@@ -36,8 +36,8 @@ let btnAddProj = document.getElementById("btnAddProj");
 let btnAPChoice = document.getElementById("btnAPChoice");
 let btnConfirmAP = document.getElementById("btnConfirmAP");
 let btnCancelAP = document.getElementById("btnCancelAP");
-let btnModProj = document.getElementById("btnModProj");
-let btnRmvProj = document.getElementById("btnRmvProj");
+// Modifier/Supprimer
+let actionsHTML = `<i class="fas fa-edit" onclick="modProj(this.id)"></i><i class="fas fa-trash-alt" onclick="rmvProj(this.id)"></i>`;
 
 // Ajout des données de l'array dans le tableau
 function createTableau() {
@@ -70,6 +70,19 @@ function createTableau() {
                 projetRow.appendChild(projetCell); // Ajout du texte dans la cellule
             }
         }
+        // Création des actions pour chaque projet
+        let projetCellActions = document.createElement("td");
+        projetCellActions.innerHTML = actionsHTML;
+        // Modification de l'id par rapport à l'id du projet
+        let PCAc = projetCellActions.children;
+        for (let o = 0; o < PCAc.length; o++) {
+            if (PCAc[o].classList.contains("fa-edit")) {
+                PCAc[o].id = `modifProj${projetsListe[i].id}`;
+            } else if (PCAc[o].classList.contains("fa-trash-alt")) {
+                PCAc[o].id = `removeProj${projetsListe[i].id}`;
+            }
+        }
+        projetRow.appendChild(projetCellActions);
     }
 }
 window.onload = createTableau(); // La fonction s'effectue au chargement de la page
@@ -82,6 +95,11 @@ function addProj() {
     // Empêche d'ajouter plusieurs fois d'un coup
     btnAddProj.style.display = "none";
     btnAPChoice.style.display = "flex";
+    // Empêche de modif ou suppr un autre projet pendant l'ajout
+    let allActionsP = document.querySelectorAll("tbody tr td i");
+    for (let prot = 0; prot<allActionsP.length; prot++) {
+        test = allActionsP[prot].style.display = "none";
+    }
 
     // Création d'une ligne dans le tableau
     let newProjRow = document.createElement("tr");
@@ -107,12 +125,17 @@ function addProj() {
         let newProjCat = `newProj${elm}`;
         newProjTextCell = document.createElement("td");
         newProjRow.appendChild(newProjTextCell);
-        newProjTCInput = document.createElement("input");
-        newProjTCInput.type = "text";
-        newProjTCInput.name = newProjCat;
-        newProjTCInput.id = newProjCat;
-        newProjTCInput.placeholder = `${elm}...`;
-        newProjTextCell.appendChild(newProjTCInput);
+        // Cellule vide pour les actions
+        if (elm == "Actions") {
+            newProjTextCell.innerHTML = "";
+        } else {
+            newProjTCInput = document.createElement("input");
+            newProjTCInput.type = "text";
+            newProjTCInput.name = newProjCat;
+            newProjTCInput.id = newProjCat;
+            newProjTCInput.placeholder = `${elm}...`;
+            newProjTextCell.appendChild(newProjTCInput);
+        }
     });
 }
 
@@ -160,24 +183,39 @@ function confirmAddProj() {
             // Remplace l'input par son contenu
             for (const [key, value] of Object.entries(projetsListe.at(-1))) {
                 if (key != "id") { // ID déjà présent donc pas modifié
-                    if (key == "url"){
+                    if (key == "url"){ // Balise a pour l'URL
                         contenuCell = `<a href="${value}" target="_blank">Cliquez ici</a>`;
                         newProjRow.children[countNPRc].innerHTML = contenuCell;
-                    } else if (key == "image") {
+                    } else if (key == "image") { // Balise img pour l'image
                         contenuCell = `<img src="${value}" alt="${value}" />`;
                         newProjRow.children[countNPRc].innerHTML = contenuCell;
-                    } else {
+                    } else { // Texte pour le reste
                         contenuCell = value;
                         newProjRow.children[countNPRc].innerHTML = contenuCell;
                     }
                     countNPRc++;
                 }
             }
+            newProjRow.children[5].innerHTML = actionsHTML; // Affiche les actions
+            let NPRlc = newProjRow.children[5].children;
+            console.log(NPRlc)
+            // Pour mettre les ID sur Modifier et Supprimer
+            for (let NPRlccount = 0 ; NPRlccount < NPRlc.length; NPRlccount++) {
+                if (NPRlc[NPRlccount].classList.contains("fa-edit")) {
+                    NPRlc[NPRlccount].id = `modifProj${newProjObj.id}`;
+                } else if (NPRlc[NPRlccount].classList.contains("fa-trash-alt")) {
+                    NPRlc[NPRlccount].id = `removeProj${newProjObj.id}`;
+                }
+            }
+            newProjRow.id = `tabProj${projetsListe.at(-1).id}`; // Pour éviter de supprimer l'ancienne ligne quand on annule après un ajout
         }
         btnAPChoice.style.display = "none"; // Les boutons Confirmer/Annuler disparaissent
         btnAddProj.style.display = "block"; // Le bouton ajouter réapparaît
-        newProjRow.id = ""; // Pour éviter de supprimer l'ancienne ligne quand on annule après un ajout
-        console.log("succès"); // Remplacer par toast
+        // Pour faire réapparaître les actions
+        let allActionsS = document.querySelectorAll("tbody tr td i");
+        for (let cb = 0; cb < allActionsS.length; cb++) {
+            allActionsS[cb].style.display = "inline-block";
+        }
     }
 }
 
@@ -186,15 +224,32 @@ btnCancelAP.addEventListener("click", () => {
     newProjRow.remove(); // La ligne du tableau est supprimée
     btnAPChoice.style.display = "none"; // Les boutons Confirmer/Annuler disparaissent
     btnAddProj.style.display = "block"; // Le bouton ajouter réapparaît
+    // Pour faire réapparaître les actions
+    let allActionsS = document.querySelectorAll("tbody tr td i");
+    for (let cb = 0; cb < allActionsS.length; cb++) {
+        allActionsS[cb].style.display = "inline-block";
+    }
 });
 
 
 // Modification projet
-function modProj() {
-    console.log(`Vous avez cliqué sur ${btnModProj.textContent}.`);
+function modProj(clicked_id) {
+    console.log(`Vous avez cliqué sur Modifier.`);
 }
 
 // Suppression projet
-function rmvProj() {
-    console.log(`Vous avez cliqué sur ${btnRmvProj.textContent}.`);
+function rmvProj(clicked_id) {
+    // Récupère la ligne (<tr>) du projet
+    let projRowToRmv = document.getElementById(clicked_id).parentNode.parentNode;
+    // Stocke l'ID dans une variable (utile pour array)
+    let IDprojToRmv = projRowToRmv.id.substr(7);
+    // Récupère l'objet dans l'array qui a cet ID
+    projetsListe.forEach(element => {
+        if(element.id == IDprojToRmv) {
+            let indexDuProjet = projetsListe.indexOf(element); // Récupère l'index du projet qui a cet ID dans l'array
+            projetsListe.splice(indexDuProjet, 1); // Supprime le projet de l'array
+        }
+    });
+    // Supprime la ligne du tableau
+    projRowToRmv.remove();
 }
